6f5ccd298b32ff53c0aa6a63238383032c067b86
skdhar50 <skdhar50@gmail.com>
2021-12-30 22:41:49 +0600

    user router added

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const userRouter = require('./database/routers/userRouter');
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/user", userRouter);
 
 module.exports = app;
---
+++ database/routers/userRouter.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const _ = require('lodash');
+const {User} = require('../models/user');
+
+const addUser = async (req, res) => {
+    let user = await User.findOne({ email: req.body.email });
+
+    if( user )
+        return res.status(401).send("User already exists!");
+
+    user = new User(_.pick(req.body, ['name','email', 'password']));
+    user.password = await bcrypt.hash(req.body.password, 10);
+
+    try {
+        const newUser = await user.save();
+        if(!newUser)
+            return res.status(401).send('User not created!');
+        const token = user.generateJWT();
+
+        return res.status(200).send({
+            token: token,
+            user: _.pick(newUser, ['_id', 'email'])
+        });
+    } catch(err) {
+        const errMsg = [];
+        for(field in err.errors) {
+            errMsg.push(err.errors[field].message);
+        }
+        return res.send(errMsg);
+    }
+}
+
+router.route("/").post(addUser);
+
+module.exports = router;
